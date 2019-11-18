c109934355971fb17f3f5ae0211e2dd82ed3b3c0
almantas88 <almantas88@gmail.com>
2019-11-18 10:30:32 +0200

    Folder hierarchy change

--- mern-exercise-tracker/server.js
+++ mern-exercise-tracker/server.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -23,6 +24,11 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
